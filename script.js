1f12748454004a93ace1e615e8a183a9556ef6a1
parvezpatwaryy <tanjilapatwaryy@gmail.com>
2026-02-22 20:51:17 +0600

    complite toggolingbtn

--- script.js
+++ script.js
@@ -6,6 +6,9 @@ let total = document.getElementById('total');
 let thrivingCount = document.getElementById('thrivingCount');
 let strugglingCount = document.getElementById('struggingCount');
 
+const allFilterBtn = document.getElementById('all-filter-btn')
+const thrivingFilterBtn = document.getElementById('thriving-filter-btn')
+const strugglingFilterBtn = document.getElementById('struggling-filter-btn')
 
 const allCardSection = document.getElementById('allCards');
 const mainContainer =  document.querySelector('main');
@@ -19,5 +22,19 @@ calculateCount()
 
 
 function toggleStyle(id){
-  console.log('click',id)
+  allFilterBtn.classList.remove('btn-primary')
+  thrivingFilterBtn.classList.remove('btn-primary')
+  strugglingFilterBtn.classList.remove('btn-primary')
+
+
+  allFilterBtn.classList.add('btn-gray','Text-black')
+  thrivingFilterBtn.classList.add('btn-gray','Text-black')
+  strugglingFilterBtn.classList.add('btn-gray','Text-black')
+  // console.log(id)
+
+
+  const selected = document.getElementById(id)
+  // console.log(selected)
+  selected.classList.remove('btn-gray')
+  selected.classList.add('btn-primary')
 }
